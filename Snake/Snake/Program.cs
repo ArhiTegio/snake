f595938dbfc822638495b8806d626a4f0387c699
Arhio <Arhio@yandex.ru>
2019-08-24 20:35:25 +0300

    Не большие изменения

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,43 +11,11 @@ namespace Snake
         static void Main(string[] args)
         {
             var point1 = new Point(1, 3, '*');
-            var point2 = new Point(4, 5, '#');
             point1.Draw();
+            var point2 = new Point(4, 5, '#');
             point2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void DrawPoint(Point point)
-        {
-            Console.SetCursorPosition(point.X, point.Y);
-            Console.Write(point.Symbol);
-        }
-    }
-
-    class Point
-    {
-        int x = 0;
-        int y = 0;
-        char symbol;
-
-        public Point() { }
-
-        public Point(int x, int y, char symbol)
-        {
-            X = x;
-            Y = y;
-            Symbol = symbol;
-        }
-
-        public int X { get => x; set => x = value; }
-        public int Y { get => y; set => y = value; }
-        public char Symbol { get => symbol; set => symbol = value; }
-
-        public void Draw()
-        {
-            Console.SetCursorPosition(this.X, this.Y);
-            Console.Write(this.Symbol);
         }
     }
 }
